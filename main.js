533409be71aec7de12b6c229ca555e3372623519
har-shit <hkchhwa@gmail.com>
2022-05-21 22:00:58 +0530

    chop cho[

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 
-
-// Get your own free OWM API key at https://www.openweathermap.org/appid - please do not re-use mine!
 // You don't need an API key for this to work at the moment, but this will change eventually.
 apik = "3045dd712ffe6e702e3245525ac7fa38"
 //kelvin to celcious
